a7713e653e797c22f0b4c3e9e833e07d8294aab4
BisonJeedom <110299206+BisonJeedom@users.noreply.github.com>
2022-11-16 19:10:31 +0100

    Add files via upload

--- desktop/js/energysaver.search.js
+++ desktop/js/energysaver.search.js
@@ -1,13 +1,26 @@
 function updateManagedEquipements(nb) {
     if(nb > 0){
         var equipements = [];
-        for (var i = 1; i <= nb; i++) {
+      	var equipementsId = [];
+      	var equipementsValue = [];
+        for (var i = 1; i <= nb; i++) {          	
+          	var value = $("#checked_input_" + i).val();
+            if (value != 0) {
+            	var id = $("#checked_input_" + i).attr('data-id');
+              	console.log("id : " + id);
+              	equipements.push([{id: id},{schedule: value}]);
+            }
+          
+          	/*
             if ($("#checked_input_" + i).is(':checked')) {
                 var id = $("#checked_input_" + i).attr('data-id');
-                equipements.push([{id: id}]);
+                //equipements.push([{id: id}]);
             }
-        }
+            */
+        }      	
 
+      	console.table(equipements);
+      	
         $.ajax({
             type: "POST",
             url: "plugins/energysaver/core/ajax/energysaver.ajax.php",
